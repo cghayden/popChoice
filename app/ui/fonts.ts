94aa91ea2f9841e89c3a8dcda7629c0834bf7cd4
Corey Hayden <cghayden@gmail.com>
2024-05-11 08:37:39 -0400

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Roboto_Slab, Carter_One } from 'next/font/google'
+
+export const roboto = Roboto_Slab({ subsets: ['latin'] })
+
+export const carterOne = Carter_One({ weight: ['400'], subsets: ['latin'] })
