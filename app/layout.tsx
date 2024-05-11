deee2ae33c082c80d6acd7db07ce188ab42d87d2
Corey Hayden <cghayden@gmail.com>
2024-05-11 11:28:36 -0400

    form UI complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { roboto } from '@/app/ui/fonts'
+import { carterOne } from '@/app/ui/fonts'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'AI Movie Recommender',
@@ -13,8 +16,32 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${roboto.className} antialiased`}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>This page has a title 🤔</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <html lang='en' className='grid place-content-center'>
+        <body className={`${roboto.className} antialiased text-white`}>
+          <main className='pt-[50px] px-8'>
+            <div className='flex flex-col items-center mb-10'>
+              <Image
+                src='/PopChoiceIcon.png'
+                width={100}
+                height={108}
+                alt='popcorn bucket logo'
+              />
+              <h1
+                className={`${carterOne.className} antialiased text-5xl mt-2`}
+              >
+                PopChoice
+              </h1>
+            </div>
+            {children}
+          </main>
+        </body>
+      </html>
+    </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,29 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Image
-        src='/PopChoiceIcon.png'
-        width={100}
-        height={108}
-        alt='popcorn bucket logo'
-      />
-    </main>
+    <form id='questions'>
+      <div className='inputItem'>
+        <label htmlFor='favorite'>What is your favorite move and why?</label>
+        <textarea name='favorite' id='favorite' rows={3}>
+          {' '}
+        </textarea>
+      </div>
+      <div className='inputItem'>
+        <label htmlFor='mood'>
+          Are you in the mood for something new or a classic?
+        </label>
+        <textarea name='mood' id='mood' rows={2}>
+          (i.e. I want to watch movies that were released after 1990)
+        </textarea>
+      </div>
+      <div className='inputItem'>
+        <label htmlFor='funOrSerious'>
+          Do you wnna have fun or do you want something serious?
+        </label>
+        <textarea name='funOrSerious' id='funOrSerious' rows={2}>
+          (i.e. I want to watch something stupid and fun)
+        </textarea>
+      </div>
+      <button>Let&apos;s Go</button>
+    </form>
   )
 }
