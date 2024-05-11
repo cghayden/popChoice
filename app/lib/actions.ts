17a9c57e64b2f9270e6bb69bf30ec9d716e45d99
Corey Hayden <cghayden@gmail.com>
2024-05-11 19:07:13 -0400

    submit prompt and queryMatch to open ai and log response

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server'
 import { z } from 'zod'
 import { openai, supabase } from '@/app/lib/config'
+import { get } from 'http'
 
 const FormSchema = z.object({
   favorite: z.string(),
@@ -28,7 +29,9 @@ export async function handleQuestionaire(formData: FormData) {
     match_threshold: 0.5,
     match_count: 1,
   })
-  console.log(data[0].content)
+  const queryMatch = data[0].content
+  const openaiRecommendation = getChatCompletion(queryMatch, query)
+
   // return the recommendation and route to the recommendation page
 }
 
@@ -43,4 +46,28 @@ async function createEmbedding(input: string) {
     console.error('Error creating embedding', error)
     throw error
   }
+}
+
+const chatMessages = [
+  {
+    role: 'system',
+    content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given two pieces of information - some context about movies and a query from a user about their interest in movies. Your job is to formulate a recommendation for a movie using the context and query.  If you cannot formulate your response from the given in the context, or if you are unsure and cannot find the answer, say, "Sorry, I don't know the answer." Please do not make up the answer. Always speak as if you were chatting to a friend.`,
+  },
+]
+
+async function getChatCompletion(queryMatch: string, userQuery: string) {
+  console.log('userQuery', userQuery)
+  console.log('queryMatch', queryMatch)
+  chatMessages.push({
+    role: 'user',
+    content: `Context: ${queryMatch} Query: ${userQuery}`,
+  })
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4-turbo',
+    messages: chatMessages,
+    temperature: 1,
+    frequency_penalty: 0.5,
+  })
+
+  console.log('openai response: ', response.choices[0].message.content)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,19 @@ export default function Home() {
     <form id='questions' action={handleQuestionaire}>
       <div className='inputItem'>
         <label htmlFor='favorite'>What is your favorite move and why?</label>
-        <textarea name='favorite' id='favorite' rows={3}>
-          {' '}
-        </textarea>
+        <textarea name='favorite' id='favorite' rows={3} />
       </div>
       <div className='inputItem'>
         <label htmlFor='mood'>
           Are you in the mood for something new or a classic?
         </label>
-        <textarea name='mood' id='mood' rows={2}></textarea>
+        <textarea name='mood' id='mood' rows={2} />
       </div>
       <div className='inputItem'>
         <label htmlFor='funOrSerious'>
           Do you wanna have fun or do you want something serious?
         </label>
-        <textarea name='funOrSerious' id='funOrSerious' rows={2}></textarea>
+        <textarea name='funOrSerious' id='funOrSerious' rows={2} />
       </div>
       <button>Let&apos;s Go</button>
     </form>
