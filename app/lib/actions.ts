c368d720dc6edd18357f79d28ed5f66224d9c2c6
Corey Hayden <cghayden@gmail.com>
2024-05-30 13:58:30 -0400

    loading state with button message

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -74,7 +74,7 @@ async function getChatCompletion(queryMatch: string, userQuery: string) {
     content: `Context: ${queryMatch} Query: ${userQuery}`,
   })
   const response = await openai.chat.completions.create({
-    model: 'gpt-4-turbo',
+    model: 'gpt-3.5-turbo',
     messages: chatMessages,
     temperature: 1,
     frequency_penalty: 0.5,
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { AIRecommendation } from './lib/types'
 // import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false)
   const [recommendation, setRecommendation] =
     useState<AIRecommendation | null>()
   const [formState, setFormState] = useState({
@@ -24,8 +25,11 @@ export default function Home() {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    setIsLoading(true)
+
     const response = await handleQuestionaire(formState)
     setRecommendation(response)
+    setIsLoading(false)
   }
 
   return recommendation ? (
@@ -33,38 +37,40 @@ export default function Home() {
   ) : (
     // <form id='questions' action={handleQuestionaire}>
     <form id='questions' onSubmit={handleSubmit}>
-      <div className='inputItem'>
-        <label htmlFor='favorite'>What is your favorite movie and why?</label>
-        <textarea
-          name='favorite'
-          id='favorite'
-          rows={3}
-          onChange={(event) => handleInputChange(event)}
-        />
-      </div>
-      <div className='inputItem'>
-        <label htmlFor='mood'>
-          Are you in the mood for something new or a classic?
-        </label>
-        <textarea
-          name='mood'
-          id='mood'
-          rows={2}
-          onChange={(event) => handleInputChange(event)}
-        />
-      </div>
-      <div className='inputItem'>
-        <label htmlFor='funOrSerious'>
-          Do you wanna have fun or do you want something serious?
-        </label>
-        <textarea
-          name='funOrSerious'
-          id='funOrSerious'
-          rows={2}
-          onChange={(event) => handleInputChange(event)}
-        />
-      </div>
-      <button>Let&apos;s Go</button>
+      <fieldset disabled={isLoading}>
+        <div className='inputItem'>
+          <label htmlFor='favorite'>What is your favorite movie and why?</label>
+          <textarea
+            name='favorite'
+            id='favorite'
+            rows={3}
+            onChange={(event) => handleInputChange(event)}
+          />
+        </div>
+        <div className='inputItem'>
+          <label htmlFor='mood'>
+            Are you in the mood for something new or a classic?
+          </label>
+          <textarea
+            name='mood'
+            id='mood'
+            rows={2}
+            onChange={(event) => handleInputChange(event)}
+          />
+        </div>
+        <div className='inputItem'>
+          <label htmlFor='funOrSerious'>
+            Do you wanna have fun or do you want something serious?
+          </label>
+          <textarea
+            name='funOrSerious'
+            id='funOrSerious'
+            rows={2}
+            onChange={(event) => handleInputChange(event)}
+          />
+        </div>
+        <button className={``}>{isLoading ? 'Thinking...' : `Let's Go`}</button>
+      </fieldset>
     </form>
   )
 }
